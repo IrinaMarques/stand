13c5fe4db2bd3965556ba1d0b07bc5d11c83d6b7
IrinaMarques <irinavpmarques@gmail.com>
2017-08-10 12:32:52 +0100

    wip dashboard lists, create a scrollbar, make styles for the viewed ones

--- src/views/dashboard/landingPage/LastMessages.jsx
+++ src/views/dashboard/landingPage/LastMessages.jsx
@@ -39,9 +39,9 @@ class LastMessages extends Component {
 								<i className="fa fa-reply" aria-hidden="true"></i>
 							</div>
 						</div>
-					</li
+					</li>
 
-					><li className='msg-list'>
+					<li className='msg-list'>
 						<div className='msg-nome'>
 							<p>Green Arrow</p>
 						</div>
@@ -63,9 +63,9 @@ class LastMessages extends Component {
 								<i className="fa fa-reply" aria-hidden="true"></i>
 							</div>
 						</div>
-					</li
+					</li>
 
-					><li className='msg-list'>
+					<li className='msg-list'>
 						<div className='msg-nome'>
 							<p>Wolverine</p>
 						</div>
@@ -87,9 +87,9 @@ class LastMessages extends Component {
 								<i className="fa fa-reply" aria-hidden="true"></i>
 							</div>
 						</div>
-					</li
+					</li>
 
-					><li className='msg-list'>
+					<li className='msg-list'>
 						<div className='msg-nome'>
 							<p>Spider Man</p>
 						</div>
@@ -112,6 +112,202 @@ class LastMessages extends Component {
 							</div>
 						</div>
 					</li>
+
+					<li className='msg-list s-grey'>
+						<div className='msg-nome nome-visto'>
+							<p>Barry Allan</p>
+						</div>
+						<div className='msg-novo msg-visto'>
+							<div className='bg-red'>
+								<p>Novo</p>
+								<i className="fa fa-exclamation" aria-hidden="true"></i>
+							</div>
+						</div>
+						<div className='msg-assunto'>
+							<p className='s-grey p-md'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada finibus magna, ut accumsan est. Suspendisse potenti.</p>
+						</div>
+						<div className='msg-data'>
+							<p className='p-sm'>03 May, 2017</p>
+						</div>
+						<div className='msg-buttons'>
+							<div className='box-buttons'>
+								<i className="fa fa-trash-o" aria-hidden="true"></i>
+								<i className="fa fa-reply" aria-hidden="true"></i>
+							</div>
+						</div>
+					</li>
+
+					<li className='msg-list s-grey'>
+						<div className='msg-nome nome-visto'>
+							<p>Green Arrow</p>
+						</div>
+						<div className='msg-novo msg-visto'>
+							<div className='bg-red'>
+								<p>Novo</p>
+								<i className="fa fa-exclamation" aria-hidden="true"></i>
+							</div>
+						</div>
+						<div className='msg-assunto'>
+							<p className='s-grey p-md'>Vivamus id eros vel justo pharetra tempor at a odio. Aliquam id cursus enim. Morbi lacinia nec neque hendrerit pharetra. </p>
+						</div>
+						<div className='msg-data'>
+							<p className='p-sm'>20 Apr, 2017</p>
+						</div>
+						<div className='msg-buttons'>
+							<div className='box-buttons'>
+								<i className="fa fa-trash-o" aria-hidden="true"></i>
+								<i className="fa fa-reply" aria-hidden="true"></i>
+							</div>
+						</div>
+					</li>
+
+					<li className='msg-list s-grey'>
+						<div className='msg-nome nome-visto'>
+							<p>Wolverine</p>
+						</div>
+						<div className='msg-novo msg-visto'>
+							<div className='bg-red'>
+								<p>Novo</p>
+								<i className="fa fa-exclamation" aria-hidden="true"></i>
+							</div>
+						</div>
+						<div className='msg-assunto'>
+							<p className='s-grey p-md'>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+						</div>
+						<div className='msg-data'>
+							<p className='p-sm'>01 Apr, 2017</p>
+						</div>
+						<div className='msg-buttons'>
+							<div className='box-buttons'>
+								<i className="fa fa-trash-o" aria-hidden="true"></i>
+								<i className="fa fa-reply" aria-hidden="true"></i>
+							</div>
+						</div>
+					</li>
+
+					<li className='msg-list s-grey'>
+						<div className='msg-nome nome-visto'>
+							<p>Spider Man</p>
+						</div>
+						<div className='msg-novo msg-visto'>
+							<div className='bg-red'>
+								<p>Novo</p>
+								<i className="fa fa-exclamation" aria-hidden="true"></i>
+							</div>
+						</div>
+						<div className='msg-assunto'>
+							<p className='s-grey p-md'>Aenean nec nunc mi. Pellentesque interdum, lorem in fermentum posuere, arcu leo luctus tortor, a dignissim ligula elit vitae lacus.</p>
+						</div>
+						<div className='msg-data'>
+							<p className='p-sm'>15 Feb, 2017</p>
+						</div>
+						<div className='msg-buttons'>
+							<div className='box-buttons'>
+								<i className="fa fa-trash-o" aria-hidden="true"></i>
+								<i className="fa fa-reply" aria-hidden="true"></i>
+							</div>
+						</div>
+					</li>
+
+					<li className='msg-list s-grey'>
+						<div className='msg-nome nome-visto'>
+							<p>Barry Allan</p>
+						</div>
+						<div className='msg-novo msg-visto'>
+							<div className='bg-red'>
+								<p>Novo</p>
+								<i className="fa fa-exclamation" aria-hidden="true"></i>
+							</div>
+						</div>
+						<div className='msg-assunto'>
+							<p className='s-grey p-md'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada finibus magna, ut accumsan est. Suspendisse potenti.</p>
+						</div>
+						<div className='msg-data'>
+							<p className='p-sm'>03 May, 2017</p>
+						</div>
+						<div className='msg-buttons'>
+							<div className='box-buttons'>
+								<i className="fa fa-trash-o" aria-hidden="true"></i>
+								<i className="fa fa-reply" aria-hidden="true"></i>
+							</div>
+						</div>
+					</li>
+
+					<li className='msg-list s-grey'>
+						<div className='msg-nome nome-visto'>
+							<p>Green Arrow</p>
+						</div>
+						<div className='msg-novo msg-visto'>
+							<div className='bg-red'>
+								<p>Novo</p>
+								<i className="fa fa-exclamation" aria-hidden="true"></i>
+							</div>
+						</div>
+						<div className='msg-assunto'>
+							<p className='s-grey p-md'>Vivamus id eros vel justo pharetra tempor at a odio. Aliquam id cursus enim. Morbi lacinia nec neque hendrerit pharetra. </p>
+						</div>
+						<div className='msg-data'>
+							<p className='p-sm'>20 Apr, 2017</p>
+						</div>
+						<div className='msg-buttons'>
+							<div className='box-buttons'>
+								<i className="fa fa-trash-o" aria-hidden="true"></i>
+								<i className="fa fa-reply" aria-hidden="true"></i>
+							</div>
+						</div>
+					</li>
+
+					<li className='msg-list s-grey'>
+						<div className='msg-nome nome-visto'>
+							<p>Wolverine</p>
+						</div>
+						<div className='msg-novo msg-visto'>
+							<div className='bg-red'>
+								<p>Novo</p>
+								<i className="fa fa-exclamation" aria-hidden="true"></i>
+							</div>
+						</div>
+						<div className='msg-assunto'>
+							<p className='s-grey p-md'>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+						</div>
+						<div className='msg-data'>
+							<p className='p-sm'>01 Apr, 2017</p>
+						</div>
+						<div className='msg-buttons'>
+							<div className='box-buttons'>
+								<i className="fa fa-trash-o" aria-hidden="true"></i>
+								<i className="fa fa-reply" aria-hidden="true"></i>
+							</div>
+						</div>
+					</li>
+
+					<li className='msg-list s-grey'>
+						<div className='msg-nome nome-visto'>
+							<p>Spider Man</p>
+						</div>
+						<div className='msg-novo msg-visto'>
+							<div className='bg-red'>
+								<p>Novo</p>
+								<i className="fa fa-exclamation" aria-hidden="true"></i>
+							</div>
+						</div>
+						<div className='msg-assunto'>
+							<p className='p-md'>Aenean nec nunc mi. Pellentesque interdum, lorem in fermentum posuere, arcu leo luctus tortor, a dignissim ligula elit vitae lacus.</p>
+						</div>
+						<div className='msg-data'>
+							<p className='p-sm'>15 Feb, 2017</p>
+						</div>
+						<div className='msg-buttons'>
+							<div className='box-buttons'>
+							
+									<i className="fa fa-trash-o" aria-hidden="true"></i>
+								
+									<i className="fa fa-reply" aria-hidden="true"></i>
+								
+							</div>
+						</div>
+					</li>
+
 				</ul>
 
                 { this.props.children() } 
