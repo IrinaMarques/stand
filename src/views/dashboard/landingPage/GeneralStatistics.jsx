e6f7cc7943b3b5a18a67318f3a687ad24c01e4d2
IrinaMarques <irinavpmarques@gmail.com>
2017-08-13 12:27:19 +0100

    wip on columns

--- src/views/dashboard/landingPage/GeneralStatistics.jsx
+++ src/views/dashboard/landingPage/GeneralStatistics.jsx
@@ -18,23 +18,27 @@ class GeneralStatistics extends Component {
 			<div>
 
 				<div className='row-visitas'>
-					<div className='col-sm-3 col-xs-6 visitas'>
-						<p>1.119</p>
-						<h3 className='p-light'>Total</h3>
-					</div>
-					<div className='col-sm-3 col-xs-6 visitas'>
-						<p>148</p>
-						<h3 className='p-light'>M&ecirc;s</h3>
-					</div>
-					<div className='row'>
-					<div className='col-sm-3 col-xs-6 visitas'>
-						<p>52</p>
-						<h3 className='p-light'>Semana</h3>
-					</div>
-					<div className='col-sm-3 col-xs-6 visitas'>
-						<p>12</p>
-						<h3 className='p-light'>Hoje</h3>
-					</div>
+					
+						<div className='col-sm-3 col-xs-6 visitas'>
+							<p>1.119</p>
+							<h3 className='p-light'>Total</h3>
+						</div>
+						<div className='col-sm-3 col-xs-6 visitas'>
+							<p>148</p>
+							<h3 className='p-light'>M&ecirc;s</h3>
+						</div>
+					
+					
+						<div className='col-sm-3 col-xs-6 visitas'>
+							<p>52</p>
+							<h3 className='p-light'>Semana</h3>
+						</div>
+						<div className='col-sm-3 col-xs-6 visitas'>
+							<p>12</p>
+							<h3 className='p-light'>Hoje</h3>
+						</div>
+					
+
 				</div>
 
                 { this.props.children() } 
