6d92971cfad3ab404acb6961de4a65fd7f19e005
MarcoPires <marcofspires@gmail.com>
2017-09-14 15:34:21 +0100

    form bottom navigation added

--- src/views/dashboard/newArticle/NewArticleBasicData.jsx
+++ src/views/dashboard/newArticle/NewArticleBasicData.jsx
@@ -18,239 +18,237 @@ class NewArticleBasicData extends Component {
 
     render() {
 		return (
-			<div>
-				<div className='msg-box'>
-
-					<div className='container-fluid'> 
+			<div className='msg-box'>
+				<form action='javascript:void(0);'>
+					<div className='container-fluid'>
 						<div className='row basic-container'>
-							<form action='javascript:void(0);'>
-								<div className='col-md-5 basic-left'>
-									<div className='row'>
-										<div className='col-xs-12 dados-nome'>
-											<TextInput 
-												id='v-nome' 
-												placeholder='Nome' 
-												labelText='Nome da Viatura' 
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
-											/>
-										</div>
-									</div>
 
-									<div className='row'>
-										<div className='col-md-6'>
-											<NumberInput 
-												id='v-preço' 
-												placeholder='00,00'
-												toFixed='2'
-												labelText='Pre&ccedil;o'
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
-											/>
-										</div>
-										<div className='col-md-6'>
-											<NumberInput 
-												id='v-kms' 
-												placeholder='0' 
-												labelText='Kms' 
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
-											/>
-										</div>
-									</div>
-
-									<div className='row'>	
-										<div className='col-md-6'>
-											<NumberInput 
-												id='v-potencia' 
-												placeholder='0' 
-												labelText='Pot&ecirc;ncia' 
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
-											/>
-										</div>
-										<div className='col-md-6'>
-											<NumberInput 
-												id='v-cilindrada' 
-												placeholder='0' 
-												labelText='Cilindrada' 
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
-											/>
-										</div>
-									</div>
-
-									<div className='row'>
-										<div className='col-md-6'>
-											<DateInput 
-												id='v-datav' 
-												placeholder='DD/MM/AA' 
-												labelText='Data do ve&iacute;culo' 
-												labelClassName='sm-date' 
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
-											/>
-										</div>
-										<div className='col-md-6'>
-											<DateInput 
-												id='v-data-e' 
-												placeholder='DD/MM/AA' 
-												labelText='Entrada no Stand' 
-												labelClassName='sm-datev' 
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
-											/>
-										</div>
+							<div className='col-md-5 basic-left'>
+								<div className='row'>
+									<div className='col-xs-12 dados-nome'>
+										<TextInput
+											id='v-nome'
+											placeholder='Nome'
+											labelText='Nome da Viatura'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
 									</div>
 								</div>
 
-								<div className='col-md-7 basic-right'>
-									<div className='row'>
-										<div className='col-md-4'>
-											<div className='col-drop'>
-												<Dropdown
-													options = {[
-														{value: 'Ligeiro'},
-														{value: 'Pesado'},
-														{value: 'Motoc&iacute;clo'}
-													]}
-													placeholder='Selecione'
-													id='v-categoria'
-													labelText='Categoria' 
-												/>
-											</div>	
-										</div>
-
-										<div className='col-md-4'>
-											<div className='col-drop'>
-												<Dropdown
-													options = {[
-														{value: 'Passageiros'},
-														{value: 'Mercadorias'},
-														{value: 'Mistos'},
-														{value: 'Tratores'},
-														{value: 'Especiais'}
-													]}
-													placeholder='Selecione'
-													id='v-sub'
-													labelText='Sub-categoria' 
-												/>
-											</div>
-										</div>	
-
-										<div className='col-md-4'>
-											<div className='col-drop'>
-												<Dropdown
-													options = {[
-														{value: 'Novo'},
-														{value: 'Como Novo'},
-														{value: 'M&iacute;stos'},
-														{value: 'Bem Conservado'},
-														{value: 'Retorma'},
-														{value: 'Outros'}
-													]}
-													placeholder='Selecione'
-													id='v-estado'
-													labelText='Estado' 
-												/>
-											</div>
-										</div>
-
+								<div className='row'>
+									<div className='col-md-6'>
+										<NumberInput
+											id='v-preço'
+											placeholder='00,00'
+											toFixed='2'
+											labelText='Pre&ccedil;o'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
 									</div>
-									<div className='row'>	
-										<div className='col-md-4'>
-											<div className='col-drop'>
-												<Dropdown
-													options = {[
-														{value: 'Gasolina'},
-														{value: 'Gas&oacute;leo'},
-														{value: 'H&iacute;brido'}
-													]}
-													placeholder='Selecione'
-													id='v-combustivel'
-													labelText='Combust&iacute;vel' 
-												/>
-											</div>
-										</div>
+									<div className='col-md-6'>
+										<NumberInput
+											id='v-kms'
+											placeholder='0'
+											labelText='Kms'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
+									</div>
+								</div>
 
-										<div className='col-md-4'>
-											<div className='col-drop'>
-												<Dropdown
-													options = {[
-														{value: 'Manual'},
-														{value: 'Manual 5 velocidades'},
-														{value: 'Manual 6 velocidades'},
-														{value: 'Híbrido'}
-													]}
-													placeholder='Selecione'
-													id='v-caixa'
-													labelText='Caixa' 
-												/>
-											</div>
-										</div>
+								<div className='row'>
+									<div className='col-md-6'>
+										<NumberInput
+											id='v-potencia'
+											placeholder='0'
+											labelText='Pot&ecirc;ncia'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
+									</div>
+									<div className='col-md-6'>
+										<NumberInput
+											id='v-cilindrada'
+											placeholder='0'
+											labelText='Cilindrada'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
+									</div>
+								</div>
 
-										<div className='col-md-4'>
-											<div className='col-drop'>
-												<Dropdown
-													options = {[
-														{value: '5 portas'},
-														{value: '3 portas'},
-														{value: '4 portas'}
-													]}
-													placeholder='Selecione'
-													id='v-portas'
-													labelText='Portas' 
-												/>
-											</div>
+								<div className='row'>
+									<div className='col-md-6'>
+										<DateInput
+											id='v-datav'
+											placeholder='DD/MM/AA'
+											labelText='Data do ve&iacute;culo'
+											labelClassName='sm-date'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
+									</div>
+									<div className='col-md-6'>
+										<DateInput
+											id='v-data-e'
+											placeholder='DD/MM/AA'
+											labelText='Entrada no Stand'
+											labelClassName='sm-datev'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
+									</div>
+								</div>
+							</div>
+
+							<div className='col-md-7 basic-right'>
+								<div className='row'>
+									<div className='col-md-4'>
+										<div className='col-drop'>
+											<Dropdown
+												options={[
+													{ value: 'Ligeiro' },
+													{ value: 'Pesado' },
+													{ value: 'Motoc&iacute;clo' }
+												]}
+												placeholder='Selecione'
+												id='v-categoria'
+												labelText='Categoria'
+											/>
 										</div>
 									</div>
 
-									<div className='row'>
-										<div className='col-md-4'>
-											<TextInput 
-												id='v-marca' 
-												placeholder='Selecione' 
-												labelText='Marca' 
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
+									<div className='col-md-4'>
+										<div className='col-drop'>
+											<Dropdown
+												options={[
+													{ value: 'Passageiros' },
+													{ value: 'Mercadorias' },
+													{ value: 'Mistos' },
+													{ value: 'Tratores' },
+													{ value: 'Especiais' }
+												]}
+												placeholder='Selecione'
+												id='v-sub'
+												labelText='Sub-categoria'
 											/>
 										</div>
-										<div className='col-md-4'>
-											<TextInput 
-												id='v-cor' 
-												placeholder='Selecione' 
-												labelText='Cor' 
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
-											/>
-										</div>	
-										<div className='col-md-4'>
-											<TextInput 
-												id='v-lugares' 
-												placeholder='Selecione' 
-												labelText='Lugares' 
-												errorClassName='error' 
-												doneClassName='done'
-												classNameState='done'
+									</div>
+
+									<div className='col-md-4'>
+										<div className='col-drop'>
+											<Dropdown
+												options={[
+													{ value: 'Novo' },
+													{ value: 'Como Novo' },
+													{ value: 'M&iacute;stos' },
+													{ value: 'Bem Conservado' },
+													{ value: 'Retorma' },
+													{ value: 'Outros' }
+												]}
+												placeholder='Selecione'
+												id='v-estado'
+												labelText='Estado'
 											/>
 										</div>
 									</div>
 
 								</div>
-							</form>
+								<div className='row'>
+									<div className='col-md-4'>
+										<div className='col-drop'>
+											<Dropdown
+												options={[
+													{ value: 'Gasolina' },
+													{ value: 'Gas&oacute;leo' },
+													{ value: 'H&iacute;brido' }
+												]}
+												placeholder='Selecione'
+												id='v-combustivel'
+												labelText='Combust&iacute;vel'
+											/>
+										</div>
+									</div>
+
+									<div className='col-md-4'>
+										<div className='col-drop'>
+											<Dropdown
+												options={[
+													{ value: 'Manual' },
+													{ value: 'Manual 5 velocidades' },
+													{ value: 'Manual 6 velocidades' },
+													{ value: 'Híbrido' }
+												]}
+												placeholder='Selecione'
+												id='v-caixa'
+												labelText='Caixa'
+											/>
+										</div>
+									</div>
+
+									<div className='col-md-4'>
+										<div className='col-drop'>
+											<Dropdown
+												options={[
+													{ value: '5 portas' },
+													{ value: '3 portas' },
+													{ value: '4 portas' }
+												]}
+												placeholder='Selecione'
+												id='v-portas'
+												labelText='Portas'
+											/>
+										</div>
+									</div>
+								</div>
+
+								<div className='row'>
+									<div className='col-md-4'>
+										<TextInput
+											id='v-marca'
+											placeholder='Selecione'
+											labelText='Marca'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
+									</div>
+									<div className='col-md-4'>
+										<TextInput
+											id='v-cor'
+											placeholder='Selecione'
+											labelText='Cor'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
+									</div>
+									<div className='col-md-4'>
+										<TextInput
+											id='v-lugares'
+											placeholder='Selecione'
+											labelText='Lugares'
+											errorClassName='error'
+											doneClassName='done'
+											classNameState='done'
+										/>
+									</div>
+								</div>
+
+							</div>
+
 						</div>
 					</div>
-
-				</div>
+				</form>
 			</div>
 		);
     }
--- src/views/dashboard/newArticle/NewArticleMoreDetails.jsx
+++ src/views/dashboard/newArticle/NewArticleMoreDetails.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import { Link } from 'react-router-dom';
 
 class NewArticleMoreDetails extends Component {
 	constructor(props) {
@@ -15,125 +15,124 @@ class NewArticleMoreDetails extends Component {
 
     render() {
 		return (
-			<div>
-				<div className='msg-box new-article-more-details'>
-					<form>
-						<div className='container-fluid'> 
-							<div className='row basic-container'>
-								<div className='col-lg-7 more-left'>
-									<div className='row'>
-										<div className='col-lg-6'>
-										<h3>Extras</h3>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='1'/>
-												<label htmlFor='1'>Ar condicionado manual</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='2'/>
-												<label htmlFor='2'>Vidros el&eacute;tricos</label>
-											</div>
-											<div className='col-xs-12 item'>	
-												<input type='checkbox' name='' id='3'/>
-												<label htmlFor='3'>4 Air Bag</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='4'/>
-												<label htmlFor='4'>Dire&ccedil;&atilde;o assistida</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='5'/>
-												<label htmlFor='5'>Fecho central de portas</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='6'/>
-												<label htmlFor='6'>Far&oacute;is de nevoeiro</label>
-											</div>
-											<div className='col-xs-12 item'>	
-												<input type='checkbox' name='' id='7'/>
-												<label htmlFor='7'>Vidros escuros</label>
-											</div>
-											<div className='col-xs-12 item'>	
-												<input type='checkbox' name='' id='8'/>
-												<label htmlFor='8'>Retrovisores aquecidos</label>
-											</div>
+			<div className='msg-box new-article-more-details'>
+				<form action='javascript:void(0);'>
+					<div className='container-fluid'> 
+						<div className='row basic-container'>
+							<div className='col-lg-7 more-left'>
+								<div className='row'>
+									<div className='col-lg-6'>
+									<h3>Extras</h3>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='1'/>
+											<label htmlFor='1'>Ar condicionado manual</label>
 										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='2'/>
+											<label htmlFor='2'>Vidros el&eacute;tricos</label>
+										</div>
+										<div className='col-xs-12 item'>	
+											<input type='checkbox' name='' id='3'/>
+											<label htmlFor='3'>4 Air Bag</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='4'/>
+											<label htmlFor='4'>Dire&ccedil;&atilde;o assistida</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='5'/>
+											<label htmlFor='5'>Fecho central de portas</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='6'/>
+											<label htmlFor='6'>Far&oacute;is de nevoeiro</label>
+										</div>
+										<div className='col-xs-12 item'>	
+											<input type='checkbox' name='' id='7'/>
+											<label htmlFor='7'>Vidros escuros</label>
+										</div>
+										<div className='col-xs-12 item'>	
+											<input type='checkbox' name='' id='8'/>
+											<label htmlFor='8'>Retrovisores aquecidos</label>
+										</div>
+									</div>
 
-										<div className='col-lg-6 right-items'>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='9'/>
-												<label htmlFor='9'>Computador de bordo</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='10'/>
-												<label htmlFor='10'>Jantes especiais</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='11'/>
-												<label htmlFor='11'>Alarme</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='12'/>
-												<label htmlFor='12'>R&aacute;dio</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='13'/>
-												<label htmlFor='13'>Sistema de ABS</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='14'/>
-												<label htmlFor='14'>Espelhos Retrovisores El&eacute;tricos</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='15'/>
-												<label htmlFor='15'>Teto de abrir el&eacute;trico</label>
-											</div>
-											<div className='col-xs-12 item'>
-												<input type='checkbox' name='' id='16'/>
-												<label htmlFor='16'>CD</label>
-											</div>
+									<div className='col-lg-6 right-items'>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='9'/>
+											<label htmlFor='9'>Computador de bordo</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='10'/>
+											<label htmlFor='10'>Jantes especiais</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='11'/>
+											<label htmlFor='11'>Alarme</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='12'/>
+											<label htmlFor='12'>R&aacute;dio</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='13'/>
+											<label htmlFor='13'>Sistema de ABS</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='14'/>
+											<label htmlFor='14'>Espelhos Retrovisores El&eacute;tricos</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='15'/>
+											<label htmlFor='15'>Teto de abrir el&eacute;trico</label>
+										</div>
+										<div className='col-xs-12 item'>
+											<input type='checkbox' name='' id='16'/>
+											<label htmlFor='16'>CD</label>
 										</div>
 									</div>
 								</div>
-								<div className='col-lg-5 more-left'>
-									<h3>Observa&ccedil;&otilde;es</h3>
-									<textarea rows="9"></textarea>
-
-									<p>Este ve&iacute;culo pertence &agrave;s novidades?</p>
-									<input type='radio' name='' id='sim-nov'/>
-									<label htmlFor='sim-nov'>Sim</label>
-									<input type='radio' name='' id='nao-nov'/>
-									<label htmlFor='nao-nov'>N&atilde;o</label>
-
-									<p>Partilhar nas redes sociais?</p>
-									<input type='radio' name='' id='sim-soc'/>
-									<label htmlFor='sim-soc'>Sim</label>
-									<input type='radio' name='' id='nao-soc'/>
-									<label htmlFor='nao-soc'>N&atilde;o</label>
-								</div>
 							</div>
+							<div className='col-lg-5 more-left'>
+								<h3>Observa&ccedil;&otilde;es</h3>
+								<textarea rows="9"></textarea>
 
-							<div className='row'>
-								<label htmlFor='outros'></label>
-								<input type='text' name='' placeholder='outros'/>
-							</div>
+								<p>Este ve&iacute;culo pertence &agrave;s novidades?</p>
+								<input type='radio' name='' id='sim-nov'/>
+								<label htmlFor='sim-nov'>Sim</label>
+								<input type='radio' name='' id='nao-nov'/>
+								<label htmlFor='nao-nov'>N&atilde;o</label>
 
-							<div className='row'>
-								<div className='col-lg-3'>
-									<button>
-										<p>Anterior</p>
-										<i className="fa fa-caret-square-o-left" aria-hidden="true"></i>
-									</button>
-								</div>
-								<div className='col-lg-3 col-lg-offset-6'>
-									<button>
-										<p>Pr&oacute;ximo</p>
-										<i className="fa fa-caret-square-o-right" aria-hidden="true"></i>
-									</button>
-								</div>
+								<p>Partilhar nas redes sociais?</p>
+								<input type='radio' name='' id='sim-soc'/>
+								<label htmlFor='sim-soc'>Sim</label>
+								<input type='radio' name='' id='nao-soc'/>
+								<label htmlFor='nao-soc'>N&atilde;o</label>
 							</div>
 						</div>
-					</form>
-				</div>
+
+						<div className='row'>
+							<label htmlFor='outros'></label>
+							<input type='text' name='' placeholder='outros'/>
+						</div>
+
+						<div className='row'>
+							<div className='col-lg-3'>
+								<Link to='/dashboard/new-article/basic-information'>
+									<p>Anterior</p>
+									<i className="fa fa-caret-square-o-left" aria-hidden="true"></i>
+								</Link>
+							</div>
+							<div className='col-lg-3 col-lg-offset-6'>
+								<Link to='/dashboard/new-article/images'>
+									<p>Pr&oacute;ximo</p>
+									<i className="fa fa-caret-square-o-right" aria-hidden="true"></i>
+								</Link>
+							</div>
+						</div>
+						
+					</div>
+				</form>
 			</div>
 		);
     }
