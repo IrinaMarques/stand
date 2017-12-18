e3d0773f86ee6ea277d696ed225096415b758587
IrinaMarques <irinavpmarques@gmail.com>
2017-12-18 18:28:12 +0000

    added some classes and removed extra info from the nav bar

--- src/components/stand/topMenu/TopMenu.jsx
+++ src/components/stand/topMenu/TopMenu.jsx
@@ -19,22 +19,21 @@ class TopMenu extends Component {
   
             <nav role='navigation'>
                 <div className='row lp'>
-                    <div className='col-md-4 col-sm-12'>
-                    </div>
-                    <ul className='col-md-8 col-sm-12'>
+                    
+                    <ul className='col-md-8 col-md-offset-4 col-sm-12'>
                         <li className='nav-menu'>
                             <i className="fa fa-bars" aria-hidden="true"></i>
                         </li>
-                        <li className=' col-sm-3 col-xs-3 col-xxs-12'>
+                        <li className=' col-sm-3 col-xxs-12'>
                             <Link to="/stand/entry">Home</Link>
                         </li>
-                        <li className=' col-sm-3 col-xs-3 col-xxs-12'>
+                        <li className=' col-sm-3 col-xxs-12'>
                             <Link to="#">Catalogo</Link>
                         </li>
-                        <li className=' col-sm-3 col-xs-3 col-xxs-12'>
+                        <li className=' col-sm-3 col-xxs-12'>
                             <Link to="#">Sobre nós</Link>
                         </li>
-                        <li className=' col-sm-3 col-xs-3 col-xxs-12'>
+                        <li className=' col-sm-3 col-xxs-12'>
                             <Link to="#">Serviços</Link>
                         </li>
                     </ul>
