09586c1e2dd1b32cae59b6a854d4673410578e71
Marco Pires <>
2017-12-19 21:15:39 +0000

    top menu toggle

--- src/components/stand/header/Header.jsx
+++ src/components/stand/header/Header.jsx
@@ -4,6 +4,9 @@ import TopMenu from '../topMenu/TopMenu';
 import Banner from '../banner/Banner';
 import Search from '../search/Search';
 
+const openClassName = 'nav-menu-open';
+const closeClassName = 'nav-menu-close';
+
 
 class Header extends Component {
     constructor(props) {
@@ -14,15 +17,30 @@ class Header extends Component {
 
     buildState(nextProps) {
         return {
+            menuIsOpen: false,
+            className: closeClassName
         }
     }
 
+    toggle() {
+        const newState = !this.state.menuIsOpen;
+
+        this.setState({
+            menuIsOpen: newState,
+            className: newState && openClassName || closeClassName
+        });
+        return this;
+    }
+
     render() {
+        const { state } = this;
+        const menuIsOpen = state.menuIsOpen;
+
         return (
-            <header role='banner'>
-                <TopMenu />
-                <Banner />
-                <Search />
+            <header role='banner' className={state.className}>
+                <TopMenu onChange={() => this.toggle()} menuIsOpen={menuIsOpen} />
+                <Banner menuIsOpen={menuIsOpen} />
+                <Search menuIsOpen={menuIsOpen} />
             </header>
         );
     }
--- src/components/stand/topMenu/TopMenu.jsx
+++ src/components/stand/topMenu/TopMenu.jsx
@@ -9,34 +9,49 @@ class TopMenu extends Component {
         return this;
     }
 
+    componentWillReceiveProps(nextProps) {
+        this.setState(this.buildState(nextProps));
+        return this;
+    }
+
     buildState(nextProps) {
+        const props = nextProps || this.props;
         return {
+            open: props.menuIsOpen || false
         }
     }
 
     render() {
+        const { open } = this.state;
+        const ImageContainer = open && (<div className='top-menu-image-container'></div>) || null;
+
         return (
   
-            <nav role='navigation' className='nav-menu-open'>
+            <nav role='navigation'>
                 <div className='row area-color'>
                     
                     <ul className='col-md-8 col-md-offset-4 col-sm-12'>
                         <li className='nav-menu col-xs-12'>
-                            <i className="fa fa-bars" aria-hidden="true"></i>
+                            <button onClick={this.props.onChange}>
+                                <i className="fa fa-bars" aria-hidden="true"></i>
+                            </button>
                         </li>
-                        <li className=' col-sm-3 col-xxs-12'>
+                        <li className='col-sm-3 col-xxs-12'>
                             <Link to="/stand/entry">Home</Link>
                         </li>
-                        <li className=' col-sm-3 col-xxs-12'>
+                        <li className='col-sm-3 col-xxs-12'>
                             <Link to="#">Catalogo</Link>
                         </li>
-                        <li className=' col-sm-3 col-xxs-12'>
+                        <li className='col-sm-3 col-xxs-12'>
                             <Link to="#">Sobre nós</Link>
                         </li>
-                        <li className=' col-sm-3 col-xxs-12'>
+                        <li className='col-sm-3 col-xxs-12'>
                             <Link to="#">Serviços</Link>
                         </li>
                     </ul>
+
+                    { ImageContainer }
+
                 </div>
             </nav>
         );
