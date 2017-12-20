8acb9de5d4d5cb876df61d2c30c976b616773a02
IrinaMarques <irinavpmarques@gmail.com>
2017-12-20 18:18:07 +0000

    Search field and button added

--- src/components/stand/search/Search.jsx
+++ src/components/stand/search/Search.jsx
@@ -16,7 +16,28 @@ class Search extends Component {
     render() {
         return (
             <div>
-               <input type="text"/>
+                <div className='search-container'>
+                    <div className='search-text'> 
+                        <h2>Pesquisa Rápida</h2>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, an usu regione definitionem, facer gloriatur at qui. Case nonumy ea duo, aliquam indoctum persecuti id has.</p>
+                    </div>
+                    <div className='search-form'>
+                        <form>
+                            <div className='inp-btt-warper'>
+                                <input type="text" placeholder='Pesquisa'/>
+                                <button classeName='Pesquisa'>
+                                    <i className="fa fa-search" aria-hidden="true"></i>
+                                </button>
+
+                                <button className='filtro'>
+                                    <i className="fa fa-sliders" aria-hidden="true"></i>
+                                </button>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
