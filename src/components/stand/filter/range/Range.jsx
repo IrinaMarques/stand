a9b28a0f93abef7dfb61751f6ed8380c5ccb42c8
IrinaMarques <irinavpmarques@gmail.com>
2018-01-22 13:31:41 +0000

    wip

--- src/components/stand/filter/range/Range.jsx
+++ src/components/stand/filter/range/Range.jsx
@@ -33,6 +33,25 @@ class Range extends Component {
                     </div>
                 </div>
 
+                <div className='range-wrapper-sm'>
+                    <div className='range-line-sm'>
+                        <div className='value'>Escolha</div>
+                        <div className='dropdown'>
+                            <i className="fa fa-chevron-down" aria-hidden="true"></i>
+                        </div>     
+                    </div>
+
+                    <span><b> a </b></span>
+
+                    <div className='range-line-sm'>
+                        <div className='value'>Escolha</div>
+                        <div className='dropdown'>
+                            <i className="fa fa-chevron-down" aria-hidden="true"></i>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         );
     }
